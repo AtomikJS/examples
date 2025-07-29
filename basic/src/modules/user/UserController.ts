df311029fa511fc9fa230714951e17758472fe8e
Rémy THIBAUT <remy.thibaut2005@gmail.com>
2025-07-29 15:36:16 +0200

    Use of @Param, @Body and @Query

--- basic/src/modules/user/UserController.ts
+++ basic/src/modules/user/UserController.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@atomikjs/http';
+import { Body, Controller, Get, Param, Post, Query } from '@atomikjs/http';
 import { UserService } from './UserService';
 
 @Controller('/users')
@@ -7,17 +7,19 @@ export class UserController {
   }
   
   @Get('/')
-  getUser() {
+  getUser(@Query('name') name: string) {
+    console.log(`Fetching user with name: ${name}`);
     return this.userService.getUser();
   }
 
   @Post('/')
-  createUser() {
+  createUser(@Body() body : any) {
+    console.log('Creating user with body:', body);
     return this.userService.createUser();
   }
 
   @Get('/:id')
-  getUserById(id: string) {
+  getUserById(@Param('id') id: string) {
     console.log(`Fetching user with ID: ${id}`);
     return this.userService.getUser(); 
   }
